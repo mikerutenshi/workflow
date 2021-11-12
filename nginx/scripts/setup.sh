66df4909f876d670744be844dabae7f168831571
emveep <michael.hermawan@emveep.com>
2021-11-12 17:15:50 +0700

    Use letsencrypt production cert

--- nginx/scripts/setup.sh
+++ nginx/scripts/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 certbot certonly -n -d $DOMAINS \
     --standalone --preferred-challenges http --email $EMAIL \
-    --agree-tos --expand --debug --staging
+    --agree-tos --expand --debug
 
 
 /usr/sbin/nginx -g "daemon off;"
